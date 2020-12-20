fde0179ec05dfd1724e6684551970e60ba255666
cmoharir <cmoharir@microstrategy.com>
2020-12-19 17:40:44 -0800

    react app to fetch user data from github and display on website using react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  const [emotion, setEmotion] = useState("Happy")
-  const [secondary, setSecondary] = useState("Tired")
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`)
-  }, [emotion])
+function App({login}) {
+const [data, setData] = useState(null);
+const [loading, setLoading] =  useState(false);
+const [error, setError] = useState(null)
 
-  useEffect (() => {
-    console.log(`It's ${secondary} around here!`)
-  }, [secondary])
+useEffect(() => {
+  if (!login)
+  return;
+  setLoading(true);
+  fetch(`https://api.github.com/users/${login}`)
+  .then((response) => response.json())
+  .then(setData)
+  .then(() => setLoading(false))
+  .catch(setError)
+},[login])
+
+if (loading) return <h1>Loading...</h1>
+if (error) return <pre>{JSON.stringify(error,null,2)}</pre>
+if (!data) return null;
+
+if (data) {
   return (
-    <>
-      <h1>Current emotion is {emotion}.</h1>
-      <button onClick = { () => setEmotion("Happy")}>Make Happy</button>
-      <button onClick = { () => setSecondary("Crabby")}>Make Crabby</button>
-      <button onClick = { () => setEmotion("Frustrated")}>Frustrate</button>
-      <button onClick = { () => setEmotion("Enthusiastic")}>Enthuse</button>
-
-    </>
-  );
+  <div>
+    <h1>{data.name}</h1>
+    <p>{data.location}</p>
+    <img alt = {data.login} src = {data.avatar_url}/>
+    
+    
+  </div>)
+}
+return (
+  <div>No user availables</div>
+      );
 }
 
 export default App;
